4a5f6a4430956b7757a980b661e432222702526e
GaloAMV-prog <galograneros19@gmail.com>
2026-01-02 22:32:52 -0300

    Fix next.config.js warning

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
     // Enable React strict mode for better development experience
     reactStrictMode: true,
-
-    // Optimize for production
-    swcMinify: true,
 }
 
 module.exports = nextConfig
